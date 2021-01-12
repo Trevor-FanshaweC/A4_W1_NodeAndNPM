f527e4a909364a669aab0f426fdaf882eb8abd6a
tvanrys <tvanrys@fanshaweonline.ca>
2021-01-12 15:04:03 -0500

    simple routes with node

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const http = require('http'); // like a PHP require
 
-// require is more or less the same as a JS import
-
-//const hostname = '127.0.0.1'; // this is localhost
-const port = process.env.PORT || 3000; // localhost:3000
+const port = process.env.PORT || 3000; // localhost:300
+const stream = require('fs');
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('8 days and counting til the World freakin parties cuz the orange idiot will be gone!');
+    res.setHeader('Content-Type', 'text/html');
+
+    let url = req.url;
+
+    switch (url){
+        case '/contact':
+            stream.readFile('contact.html', null, ((err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write('404 not found');
+                } else {
+                    res.write(data);
+                }
+            }))
+        break;
+
+        case '/contact':
+            stream.readFile('contact.html', null, ((err, data) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.write('404 not found');
+                } else {
+                    res.write(data);
+                }
+            }))
+        break;
+
+        default:
+        res.end('8 days and counting til the World freakin parties cuz the orange idiot will be gone!');
+    }    
 });
 
 server.listen(port, () => {
